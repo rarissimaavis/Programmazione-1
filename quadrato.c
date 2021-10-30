278701ba6bba7abe42ed2abfc7461179a480006f
rarissimaavis <85623941+rarissimaavis@users.noreply.github.com>
2021-10-30 22:56:34 +0200

    Add files via upload

---
+++ quadrato.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+void quadrato(int n) 
+{
+	int i, j;
+	i = 1;
+	// stampa n righe
+	while(i <= n) 
+	{
+		// stampa una riga
+		j = 1;
+		while(j <= n) 
+		{
+			printf("* ");
+			++j;
+		}
+		printf("\n");
+		++i;
+	}
+}
+int main(void)
+{
+	int lato;
+	printf("lato del quadrato? ");
+	scanf("%d", &lato);
+	quadrato(lato);
+	return 0;
+}
+
+
